9c56eda3b93b1295cce6338d20fc419d141fc728
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-22 17:47:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-  attr_accessor :size :color :properties :material :
+  attr_accessor :size :color :properties :material :condition :cobbler
   def initialize(brand)
     @brand = brand
   end
